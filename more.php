c8b345e87abd6929f0564802b831f2353724638d
alexey <alexey.serbi@mail.com>
2018-07-04 19:37:57 +0300

    1 commit

---
+++ more.php
@@ -0,0 +1,8 @@
+<?php require_once ("header.php"); ?>
+
+<p align="left">  <img src="img/img.2" alt="img"  height="800" width="400" class="img-thumbnail"></p>
+
+
+<?php require_once ("footer.php"); ?>
+
+
