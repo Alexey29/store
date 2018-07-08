d1832f0aa66b81587aaccad82dbd973816596f8f
Alex Serbin <alexey.serbi@gmail.com>
2018-07-09 00:29:32 +0300

    work wiht homework

--- resources/session.php
+++ resources/session.php
@@ -37,18 +37,31 @@
 <!--//session_start();-->
 <!--//var_dump(session_get_cookie_params());-->
 
-<form action="session2.php" method="post">
+<!--<form action="session2.php" method="post">-->
+<!---->
+<!--<div class="input-group">-->
+<!--  <span class="input-group-addon" id="basic-addon1">@</span>-->
+<!--  <input type="text" class="form-control" placeholder="Username" aria-describedby="basic-addon1" name="email">-->
+<!--</div>-->
+<!--<br><br>-->
+<!---->
+<!--<div class="input-group">-->
+<!--  <span class="input-group-addon">$</span>-->
+<!--  <input type="text" class="form-control" aria-label="Amount (to the nearest dollar)" name="passvord">-->
+<!--  <span class="input-group-addon">.00</span>-->
+<!--</div>-->
+<!--    <button type="submit">Submit</button>-->
+<!--</form>-->
 
-<div class="input-group">
-  <span class="input-group-addon" id="basic-addon1">@</span>
-  <input type="text" class="form-control" placeholder="Username" aria-describedby="basic-addon1" name="email">
-</div>
-<br><br>
 
-<div class="input-group">
-  <span class="input-group-addon">$</span>
-  <input type="text" class="form-control" aria-label="Amount (to the nearest dollar)" name="passvord">
-  <span class="input-group-addon">.00</span>
-</div>
-    <button type="submit">Submit</button>
+<form action="../resources/session2.php">
+    <div class="form-group">
+        <label for="exampleInputEmail1">Email address</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+    </div>
+    <div class="form-group">
+        <label for="exampleInputPassword1">Password</label>
+        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+    </div>
+    <button type="submit" class="btn btn-default">Submit</button>
 </form>
--- resources/session2.php
+++ resources/session2.php
@@ -37,23 +37,37 @@
 <!--//session_start();-->
 <!--//var_dump(session_get_cookie_params());-->
 
-<form action="session2.php" method="post">
-
-    <div class="input-group">
-        <span class="input-group-addon" id="basic-addon1">@</span>
-        <input type="text" class="form-control" placeholder="Username" aria-describedby="basic-addon1" name="email">
-    </div>
-    <br><br>
-
-    <div class="input-group">
-        <span class="input-group-addon">$</span>
-        <input type="text" class="form-control" aria-label="Amount (to the nearest dollar)" name="passvord">
-        <span class="input-group-addon">.00</span>
-    </div>
-    <button type="submit">Submit</button>
-</form>
+<!--<form action="session2.php" method="post">-->
+<!---->
+<!--    <div class="input-group">-->
+<!--        <span class="input-group-addon" id="basic-addon1">@</span>-->
+<!--        <input type="text" class="form-control" placeholder="Username" aria-describedby="basic-addon1" name="email">-->
+<!--    </div>-->
+<!--    <br><br>-->
+<!---->
+<!--    <div class="input-group">-->
+<!--        <span class="input-group-addon">$</span>-->
+<!--        <input type="text" class="form-control" aria-label="Amount (to the nearest dollar)" name="passvord">-->
+<!--        <span class="input-group-addon">.00</span>-->
+<!--    </div>-->
+<!--    <button type="submit">Submit</button>-->
+<!--</form>-->
 
 <?php
+
+
+$A = new Authentication();
+
+if($A->isAuth()){
+    echo $A->getlogin();
+}else{
+    $A->auth();
+}
+//echo $A->getlogin();
+//$A->logout();
+
+
+
 Class Authentication{
     private $UserNaim;
 
@@ -61,8 +75,8 @@ Class Authentication{
 
     public function __construct()
     {
-        $this->UserNaim="user";
-        $this->login="login";
+        $this->UserNaim = "user";
+        $this->login = "login";
     }
 
 
@@ -81,8 +95,8 @@ Class Authentication{
                 throw new Exception("you are already authorized!");
         } else{
                 session_start();
-               $_SESSION["user"]=$this->UserNaim;
-               $_SESSION["login"]=$this->login;
+               $_SESSION["user"] = $this->UserNaim;
+               $_SESSION["login"] = $this->login;
         }
         }catch (Exception $e){
             echo $e->getMessage();
@@ -107,7 +121,7 @@ Class Authentication{
             if(!($this->isAuth())){
                 throw new Exception("you are't authorized!");
             }else{
-                $_SESSION=arrey();
+                $_SESSION=array();
             }
         }catch (Exception $e){
             echo $e->getMessage();
